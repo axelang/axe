6cb0328258ac00b7774efd2103bf104d31801785
Navid M <navidmomtahen@gmail.com>
2025-11-14 19:32:45 +0000

    fix

--- source/axe/imports.d
+++ source/axe/imports.d
@@ -22,6 +22,7 @@ ASTNode processImports(ASTNode ast, string baseDir, bool isAxec)
 
     ASTNode[] newChildren;
     string[string] importedFunctions;
+    string[string] importedModels;  // Track model renames
 
     foreach (child; programNode.children)
     {
@@ -63,6 +64,9 @@ ASTNode processImports(ASTNode ast, string baseDir, bool isAxec)
             auto importAst = parse(importTokens, true);
             auto importProgram = cast(ProgramNode) importAst;
 
+            // Replace slashes with underscores for valid C identifiers
+            string sanitizedModuleName = useNode.moduleName.replace("/", "_");
+
             foreach (importChild; importProgram.children)
             {
                 if (importChild.nodeType == "Function")
@@ -70,8 +74,6 @@ ASTNode processImports(ASTNode ast, string baseDir, bool isAxec)
                     auto funcNode = cast(FunctionNode) importChild;
                     if (useNode.imports.canFind(funcNode.name))
                     {
-                        // Replace slashes with underscores for valid C identifiers
-                        string sanitizedModuleName = useNode.moduleName.replace("/", "_");
                         string prefixedName = sanitizedModuleName ~ "_" ~ funcNode.name;
                         importedFunctions[funcNode.name] = prefixedName;
                         auto newFunc = new FunctionNode(prefixedName, funcNode.params);
@@ -85,7 +87,11 @@ ASTNode processImports(ASTNode ast, string baseDir, bool isAxec)
                     auto modelNode = cast(ModelNode) importChild;
                     if (useNode.imports.canFind(modelNode.name))
                     {
-                        newChildren ~= modelNode;
+                        string prefixedName = sanitizedModuleName ~ "_" ~ modelNode.name;
+                        importedModels[modelNode.name] = prefixedName;
+                        auto newModel = new ModelNode(prefixedName, null);
+                        newModel.fields = modelNode.fields;
+                        newChildren ~= newModel;
                     }
                 }
             }
@@ -93,6 +99,7 @@ ASTNode processImports(ASTNode ast, string baseDir, bool isAxec)
         else
         {
             renameFunctionCalls(child, importedFunctions);
+            renameTypeReferences(child, importedModels);
             newChildren ~= child;
         }
     }
@@ -129,6 +136,85 @@ void renameFunctionCalls(ASTNode node, string[string] nameMap)
     foreach (child; node.children)
     {
         renameFunctionCalls(child, nameMap);
+    }
+}
+
+/**
+ * Recursively rename type references to use prefixed model names
+ */
+void renameTypeReferences(ASTNode node, string[string] typeMap)
+{
+    import std.algorithm : startsWith;
+    import std.array : split, join;
+    
+    if (node.nodeType == "Function")
+    {
+        auto funcNode = cast(FunctionNode) node;
+        
+        // Update return type
+        if (funcNode.returnType in typeMap)
+        {
+            funcNode.returnType = typeMap[funcNode.returnType];
+        }
+        
+        // Update parameter types
+        for (size_t i = 0; i < funcNode.params.length; i++)
+        {
+            string param = funcNode.params[i];
+            auto parts = param.split(" ");
+            if (parts.length >= 2)
+            {
+                // Handle "Type name" or "Type[] name" or "ref Type name"
+                foreach (oldType, newType; typeMap)
+                {
+                    // Replace standalone type names
+                    if (parts[0] == oldType)
+                    {
+                        parts[0] = newType;
+                    }
+                    // Handle ref types
+                    else if (parts.length >= 3 && parts[0] == "ref" && parts[1] == oldType)
+                    {
+                        parts[1] = newType;
+                    }
+                    // Handle array types
+                    else if (parts[0].startsWith(oldType ~ "["))
+                    {
+                        parts[0] = parts[0].replace(oldType ~ "[", newType ~ "[");
+                    }
+                }
+                funcNode.params[i] = parts.join(" ");
+            }
+        }
+    }
+    else if (node.nodeType == "Declaration")
+    {
+        auto declNode = cast(DeclarationNode) node;
+        if (declNode.typeName in typeMap)
+        {
+            declNode.typeName = typeMap[declNode.typeName];
+        }
+    }
+    else if (node.nodeType == "ArrayDeclaration")
+    {
+        auto arrDeclNode = cast(ArrayDeclarationNode) node;
+        if (arrDeclNode.elementType in typeMap)
+        {
+            arrDeclNode.elementType = typeMap[arrDeclNode.elementType];
+        }
+    }
+    else if (node.nodeType == "ModelInstantiation")
+    {
+        auto modelInstNode = cast(ModelInstantiationNode) node;
+        if (modelInstNode.modelName in typeMap)
+        {
+            modelInstNode.modelName = typeMap[modelInstNode.modelName];
+        }
+    }
+
+    foreach (child; node.children)
+    {
+        renameTypeReferences(child, typeMap);
     }
 }
 
