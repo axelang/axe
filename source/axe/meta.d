d35f5297f69c686c5584d6e1e0f23a2ca1052651
Navid M <navidmomtahen@gmail.com>
2025-11-22 13:03:56 +0000

    fmt

--- source/axe/meta.d
+++ source/axe/meta.d
@@ -1,6 +1,6 @@
 /** 
  * Axe Programming Language Compiler.
- * Author: Navid M (C) 2025
+ * Author: Navid Momtahen (C) 2025
  * License: GPL-3.0
  * 
  * Contains meta information about the compiler.
@@ -9,4 +9,4 @@
 module axe.meta;
 
 auto ver = "Axe v0.0.1";
-auto about = "Specification and compiler by Navid M ((C) GPL-3.0, 2025)";
+auto about = "Specification and compiler by Navid Momtahen ((C) GPL-3.0, 2025)";
